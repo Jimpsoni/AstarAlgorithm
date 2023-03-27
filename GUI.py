f5b4a8a17df9798536d5b599388470a65de89d1a
Jimi Jukkala <jukkalajimi@gmail.com>
2023-03-28 00:21:44 +0300

    Improved the overall quality of the code

--- GUI.py
+++ GUI.py
@@ -1,117 +1,123 @@
-import pygame as pg
 import time
 from Algorithm import AStar, Node
 
-pg.init()
-
-# ===============================================
-#              Variables
-
-# Screen, width and height
-WIDTH, HEIGHT = 1200, 900
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("A* path solving algorithm")
-
-# All the colors
-checked = pg.color.Color(243, 62, 62)
-nodes = pg.color.Color(84, 212, 72)
-unchecked = pg.color.Color(255, 255, 255)
-start_point_color = pg.color.Color(230, 140, 10)
-end_point_color = pg.color.Color(148, 13, 40)
-not_traversable = pg.color.Color(16, 38, 68)
-
-# init the game
-game = AStar(Node(0, 0), Node(49, 49), 50, 50)
-
-# Everything needed to draw the tiles to screen
-start_point = WIDTH - HEIGHT
-between_tiles = 1
-
-tile_width = HEIGHT / game.width - 2 * between_tiles
-tile_height = HEIGHT / game.height - 2 * between_tiles
+try:
+    import pygame as pg
+except ImportError as e:
+    print("Couldn't import " + e.name)
+    import pygame as pg
 
 
-# ===============================================
-#              Functions
+class visualizer:
 
-def draw_tile(x, y, color):
-    """ Draws a single tile """
-    x_coord = start_point + between_tiles + (tile_width + 2 * between_tiles) * x
-    y_coord = between_tiles + (tile_width + 2 * between_tiles) * y
-    pg.draw.rect(screen, color, (x_coord, y_coord, tile_width, tile_height))
+    # Not initialized yet
+    screen = None
 
+    # All the colors we need
+    nodes = pg.color.Color(84, 212, 72)
+    unchecked = pg.color.Color(255, 255, 255)
+    start_point_color = pg.color.Color(230, 140, 10)
+    end_point_color = pg.color.Color(148, 13, 40)
+    not_traversable = pg.color.Color(16, 38, 68)
+    checked = pg.color.Color(228, 79, 79)
 
-def set_up():
-    """
-    Sets up the screen, draws rectangles which represents the tiles on game board
-    """
-    # Draw all the tiles
-    for i in range(0, game.height):
-        for j in range(0, game.width):
-            draw_tile(i, j, unchecked)
+    between_tiles = 1
 
-    # Draw all non-traversable tiles
-    for row in game.board:
-        for node in row:
-            if not node.traversable:
-                draw_tile(node.x, node.y, not_traversable)
+    game = AStar(Node(0, 0), Node(24, 24), 25, 25)
 
-    # Draw start and end point
-    draw_tile(game.start.x, game.start.y, start_point_color)
-    draw_tile(game.end.x, game.end.y, end_point_color)
+    def __init__(self, width: int, height: int) -> None:
+        pg.init()
+        self.width, self.height = width, height
+        self.start_point = self.width - self.height
+        self.tile_width = self.height / self.game.width - 2 * self.between_tiles
+        self.tile_height = self.height / self.game.height - 2 * self.between_tiles
 
+    def set_display(self) -> None:
+        self.screen = pg.display.set_mode((self.width, self.height))
+        pg.display.set_caption("A* path solving algorithm")
 
-def draw_route(shortest_route):
-    """ Draws the final route to destination """
-    color = pg.color.Color(155, 155, 155)
-    for n in shortest_route:
-        draw_tile(n.x, n.y, color)
+    def draw_tile(self, x: float, y: float, color: pg.color) -> None:
+        """ Draws a single tile """
+        x_coord = self.start_point + self.between_tiles + (self.tile_width + 2 * self.between_tiles) * x
+        y_coord = self.between_tiles + (self.tile_width + 2 * self.between_tiles) * y
+        pg.draw.rect(self.screen, color, (x_coord, y_coord, self.tile_width, self.tile_height))
 
-    pg.display.update()
+    def set_up(self) -> None:
+        """
+        Sets up the screen, draws rectangles which represents the tiles on game board
+        """
+        # Draw all the tiles
+        for i in range(0, self.game.height):
+            for j in range(0, self.game.width):
+                self.draw_tile(i, j, self.unchecked)
 
+        # Draw all non-traversable tiles
+        for row in self.game.board:
+            for node in row:
+                if not node.traversable:
+                    self.draw_tile(node.x, node.y, self.not_traversable)
 
-def draw_screen():
-    """
-    Gets the checked nodes and current nodes from Astar class and draws them
-    """
-    for n in game.nodes:
-        draw_tile(n.x, n.y, nodes)
+        # Draw start and end point
+        self.draw_tile(self.game.start.x, self.game.start.y, self.start_point_color)
+        self.draw_tile(self.game.end.x, self.game.end.y, self.end_point_color)
 
-    for n in game.checked:
-        draw_tile(n.x, n.y, checked)
+    def draw_route(self, shortest_route: list[Node]) -> None:
+        """ Draws the final route to destination """
+        color = pg.color.Color(155, 155, 155)
+        for n in shortest_route:
+            self.draw_tile(n.x, n.y, color)
 
-    pg.display.update()
+        pg.display.update()
+
+    def draw_screen(self) -> None:
+        """
+        Gets the checked nodes and current nodes from Astar class and draws them
+        """
+        # Draw all the neighbors
+        for n in self.game.nodes:
+            self.draw_tile(n.x, n.y, self.nodes)
+
+        # Draw all the checked tiles
+        for n in self.game.checked:
+            self.draw_tile(n.x, n.y, self.checked)
+
+        pg.display.update()
+
+    def run(self):
+        running = True
+
+        # Create a map
+        self.game.board[1][1].traversable = False
+        self.game.board[1][2].traversable = False
+        self.game.board[2][3].traversable = False
+        self.game.board[3][3].traversable = False
+        self.game.board[4][3].traversable = False
+        self.game.board[1][4].traversable = False
+
+        self.set_up()
+
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    break
+
+            # When game iteration returns something else than none
+            # it has found the path
+            path = self.game.iteration()
+            if path is not None:
+                self.set_up()
+                self.draw_route(path)
+                time.sleep(3)
+                break
+
+            # Draws everything on screen
+            self.draw_screen()
+
+            pg.time.wait(10)
 
 
 if __name__ == "__main__":
-    running = True
-
-    # Create a map
-    game.board[1][1].traversable = False
-    game.board[1][2].traversable = False
-    game.board[2][3].traversable = False
-    game.board[3][3].traversable = False
-    game.board[4][3].traversable = False
-    game.board[1][4].traversable = False
-
-    set_up()
-
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                break
-
-        # When game iteration returns something else than none
-        # it has found the path
-        path = game.iteration()
-        if path is not None:
-            set_up()
-            draw_route(path)
-            time.sleep(3)
-            break
-
-        # Draws everything on screen
-        draw_screen()
-
-        time.sleep(0.1)
+    visualizer = visualizer(1200, 900)
+    visualizer.set_display()
+    visualizer.run()
