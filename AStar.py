c3d8ba74e484e5550903ec44ef4b73e6556a9615
Jimi Jukkala <jukkalajimi@gmail.com>
2023-04-03 20:19:00 +0300

    Moved Node class to its own file and used algorithm superclass

--- AStar.py
+++ AStar.py
@@ -1,6 +1,7 @@
-from __future__ import annotations
-from math import sqrt, floor
 from operator import attrgetter
+from Algorithm import Algorithm
+from Node import Node
+
 
 """
 RULES OF A* ALGORITHM
@@ -12,53 +13,7 @@ TODO - List
 """
 
 
-class Node:
-    # Initialize cost variables to max int32 values because we probably don't have that big board
-    # 2**32 = 4294967296 so the board would be over 65536 nodes in width
-    gcost = 2**32-1
-    hcost = 2**32-1
-    fcost = 2**32-1
-
-    previous_node = None
-    traversable = True
-
-    def __init__(self, x: int, y: int) -> None:
-        """ To create a node we need an x and a y coordinates """
-        self.x = x
-        self.y = y
-
-    def calculate_values(self, start_node: Node, end_node: Node) -> None:
-        """
-        Calculates the hcost, gcost and fcost using nodes own x and y and additional
-        parameters "end_node" and "start_node" and calculating the distance to those points
-        from nodes own coordinates
-        :param start_node node where we entered this current node
-        :param end_node same as start_node, but with endpoint coordinates
-        """
-        self.gcost = start_node.gcost + floor(sqrt((self.x - start_node.x) ** 2 + (self.y - start_node.y) ** 2) * 10)
-        self.hcost = floor(sqrt((self.x - end_node.x) ** 2 + (self.y - end_node.y) ** 2) * 10)
-        self.fcost = self.gcost + self.hcost
-        self.previous_node = start_node
-
-    def gcost_to(self, node: Node) -> int:
-        """ returns the gcost to a node """
-        return floor(sqrt((self.x - node.x) ** 2 + (self.y - node.y) ** 2) * 10)
-
-    def set_as_start(self) -> None:
-        self.gcost = 0
-
-    def change_traversable(self):
-        self.traversable = not self.traversable
-
-    def __str__(self) -> str:
-        return f"Node({self.x}, {self.y})"
-
-    def __eq__(self, node) -> bool:
-        """ Checking if two nodes are equal """
-        return (self.x == node.x) and (self.y == node.y)
-
-
-class AStar:
+class AStar(Algorithm):
     nodes = []
     checked = []
 
@@ -79,50 +34,10 @@ class AStar:
         self.width = width
         self.height = height
 
-        self.board = self.make_board()
+        super().__init__(width, height)
+
         self.board[self.start.y][self.start.x].set_as_start()
         self.nodes.append(start)
-
-
-    """
-    def get_neighbors(self, node: Node) -> list[Node]:
-        Returns all the available neighbors of the given node. This excludes all the nodes that are not traversable
-        and the centerpiece.
-        :param node: The node of which neighbors we get
-        
-        nodes = []  # Here we append all the neighbors
-
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                # If we are out of the board, or we are at centerpiece
-                if 0 <= node.y + i < self.height and 0 <= node.x + j < self.width and not (i == 0 and j == 0):
-                    # If the node is traversable
-                    if self.board[node.y + i][node.x + j].traversable:
-                        nodes.append(self.board[node.y + i][node.x + j])
-
-        return nodes
-    """
-    def get_neighbors(self, node):
-        nodes = []  # Here we append all the neighbors
-
-        if node.x + 1 < len(self.board[0]):
-            if self.board[node.y][node.x + 1].traversable:
-                nodes.append(self.board[node.y][node.x + 1])
-
-        if node.x - 1 >= 0:
-            if self.board[node.y][node.x - 1].traversable:
-                nodes.append(self.board[node.y][node.x - 1])
-
-        if node.y + 1 < len(self.board):
-            if self.board[node.y + 1][node.x].traversable:
-                nodes.append(self.board[node.y + 1][node.x])
-
-        if node.y - 1 >= 0:
-            if self.board[node.y - 1][node.x].traversable:
-                nodes.append(self.board[node.y - 1][node.x])
-
-        return nodes
-
 
     def give_values(self, node: Node, current: Node) -> None:
         """
@@ -132,12 +47,6 @@ class AStar:
         :return:
         """
         node.calculate_values(current, self.end)
-
-    def make_board(self) -> list[list[Node]]:
-        """
-        :return: 2D list that has height amount of lists size of the width full of nodes.
-        """
-        return [[Node(i, j) for i in range(0, self.width)] for j in range(0, self.height)]
 
     def run(self) -> list[Node]:
         """
@@ -151,6 +60,10 @@ class AStar:
             path = self.iteration()
 
         return path
+
+    @staticmethod
+    def validate_neighbor(node):
+        return node.traversable
 
     def iteration(self) -> list[Node]:
         """
@@ -167,12 +80,12 @@ class AStar:
         if current == self.end:
             return self.get_path(current)
 
-        for neighbor in self.get_neighbors(current):
+        for neighbor in self.get_neighbors(current, 1, self.validate_neighbor):
             if neighbor in self.checked:
                 continue
 
             # If the neighbor is not in the nodes, or we find a shorter path to the node
-            if neighbor not in self.nodes or neighbor.gcost > (current.gcost + current.gcost_to(neighbor)):
+            if neighbor not in self.nodes or neighbor.gcost > (current.gcost + current.gcost_to()):
                 # Then we calculate the path to this node
                 neighbor.calculate_values(current, self.end)
                 neighbor.previous = current
@@ -193,18 +106,29 @@ class AStar:
             current = current.previous_node
         return path
 
-    def print_board(self) -> None:
-        """ Prints the board in to the console """
-        for row in self.board:
-            for node in row:
-                print(str(node) + "  ", end="")
-            print("\n")
-
     def __str__(self) -> str:
-        """ Return string representation of the board """
         return str([str(line) + "\n" for line in self.board])
 
 
 if __name__ == "__main__":
     print("This is the AStar algorithm\n")
     print("Rules are simple:")
+
+    """
+    def get_neighbors(self, node: Node) -> list[Node]:
+        Returns all the available neighbors of the given node. This excludes all the nodes that are not traversable
+        and the centerpiece.
+        :param node: The node of which neighbors we get
+
+        nodes = []  # Here we append all the neighbors
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                # If we are out of the board, or we are at centerpiece
+                if 0 <= node.y + i < self.height and 0 <= node.x + j < self.width and not (i == 0 and j == 0):
+                    # If the node is traversable
+                    if self.board[node.y + i][node.x + j].traversable:
+                        nodes.append(self.board[node.y + i][node.x + j])
+
+        return nodes
+    """
